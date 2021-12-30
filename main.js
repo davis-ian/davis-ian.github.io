557a2e0be8bd8447c734bd89a6b5931133d8f8df
Ian Davis <iandavis6481@gmail.com>
2021-12-30 13:48:23 -0800

    scroll fix

--- main.js
+++ main.js
@@ -1,87 +1,70 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
+function init() {
+    gsap.from('.greet h1', {
+        opacity: 0,
+        xPercent: 50,
+        duration: 2,
+        ease: "power2"
+    })
+    gsap.from('.welcome p', {
+        opacity: 0,
+        y: 200,
+        duration: 1,
+        ease: 'power2',
+        stagger: .5
+    })
 
-
-gsap.from(".greet h1", {
-    ease: "power4", 
-    xPercent: 70, 
-    duration:2,
-    opacity:0
-})
-gsap.from(".welcome p", {
-    ease:"power2", 
-    y:200, 
-    opacity: 0, 
-    duration: 2, 
-    stagger: .2, 
-    delay: .5
-})
-
-gsap.from(".awrapper", {
-    ease: "power2",
-    x:-500,
-    immediateRender: false,
-    opacity: 0, 
-    scrollTrigger: {
+    gsap.from('.awrapper', {
+        opacity: 0, 
+        xPercent: -50,
+        scrollTrigger: {
         trigger:".awrapper",
-        scrub: 1,
-        start: "top 80%",
-        end: "bottom center",
-        
-    },
-    
-})
-gsap.from("#p1", {
-    scrollTrigger: {
-        trigger: "#p1",
-        scrub: 1,
-        start: "top bottom",
-        end: "center center"
-    },
-    ease: "power2",
-    x:500,
-    immediateRender: false,
-    opacity: 0, 
-    
-})
-gsap.from("#p2", {
-    scrollTrigger: {
-        trigger: "#p2",
-        scrub: 1,
-        start: "top bottom",
-        end: "center center"
-    },
-    ease: "power2",
-    x:-500,
-    immediateRender: false,
-    opacity: 0, 
-    
-})
-gsap.from("#p3", {
-    scrollTrigger: {
-        trigger: "#p3",
-        scrub: 1,
-        start: "top bottom",
-        end: "center center"
-    },
-    ease: "power2",
-    x:500,
-    immediateRender: false,
-    opacity: 0, 
-    
-})
-gsap.from(".contact h1", {
-    scrollTrigger: {
-        trigger: ".contact",
-        scrub: 1,
+        scrub:1,
         start: "top bottom",
         end: "center center",
-              
-    },
-    ease: "power2",
-    y:500,
-    immediateRender: false,
-    opacity: 0, 
-})
+    }})
+    gsap.from('#p1', {
+        opacity: 0, 
+        xPercent: 50,
+        scrollTrigger: {
+        trigger:"#p1",
+        scrub:1,
+        start: "top bottom",
+        end: "center center",
+    }})
+    gsap.from('#p2', {
+        opacity: 0, 
+        xPercent: -50,
+        scrollTrigger: {
+        trigger:"#p2",
+        scrub:1,
+        start: "top bottom",
+        end: "center center",
+    }})
+    gsap.from('#p3', {
+        opacity: 0, 
+        xPercent: 50,
+        scrollTrigger: {
+        trigger:"#p3",
+        scrub:1,
+        start: "top bottom",
+        end: "center center",
+    }})
+    gsap.from('.contact h1', {
+        opacity:0,
+        y: 500,
+        scrollTrigger: {
+            trigger: '.contact',
+            start: "top bottom",
+            end: "center center",
+            scrub:1,
+            
+        }
+    })
+}
 
+window.addEventListener('load', function() {
+    init()
+})
