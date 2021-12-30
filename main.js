568cf070e65fc1c0c9e1c4ce58506920b9a4c786
Ian Davis <iandavis6481@gmail.com>
2021-12-30 08:21:38 -0800

    animation fix immediate render

--- main.js
+++ main.js
@@ -17,3 +17,67 @@ gsap.from(".welcome p", {
     stagger: .2, 
     delay: .5
 })
+
+gsap.from(".awrapper", {
+    ease: "power2",
+    x:-500,
+    immediateRender: false,
+    opacity: 0, 
+    scrollTrigger: {
+        trigger:".awrapper",
+        scrub: 1,
+        start: "top 80%",
+        end: "bottom center",
+    },
+    
+})
+gsap.from("#p1", {
+    scrollTrigger: {
+        trigger: "#p1",
+        scrub: 1,
+        start: "top bottom",
+        end: "center center"
+    },
+    ease: "power2",
+    x:500,
+    immediateRender: false,
+    opacity: 0, 
+    
+})
+gsap.from("#p2", {
+    scrollTrigger: {
+        trigger: "#p2",
+        scrub: 1,
+        start: "top bottom",
+        end: "center center"
+    },
+    ease: "power2",
+    x:-500,
+    immediateRender: false,
+    opacity: 0, 
+    
+})
+gsap.from("#p3", {
+    scrollTrigger: {
+        trigger: "#p3",
+        scrub: 1,
+        start: "top bottom",
+        end: "center center"
+    },
+    ease: "power2",
+    x:500,
+    immediateRender: false,
+    opacity: 0, 
+    
+})
+gsap.from(".contact h1", {
+    scrollTrigger: {
+        trigger: ".contact h1",
+        toggleActions: "play complete pause none",
+    },
+    ease: "power2",
+    y:500,
+    immediateRender: false,
+    opacity: 0, 
+})
+
